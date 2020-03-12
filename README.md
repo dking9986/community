0d56db1fea6f61b817644e75ede948c0d30a2c4a
dking9986 <565172031@qq.com>
2020-03-12 17:48:05 +0800

    modify bug

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 [github 登录]{https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/}
 [bootstrap]{https://v3.bootcss.com/getting-started/}
 ##工具
-git
+[git]
+[flyway]
 ##脚本
 ```sql
 create table USER
--- src/main/java/note/dk/community/community/controller/IndexController.java
+++ src/main/java/note/dk/community/community/controller/IndexController.java
@@ -4,10 +4,7 @@ import note.dk.community.community.mapper.UserMapper;
 import note.dk.community.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,19 +12,19 @@ import javax.servlet.http.HttpServletRequest;
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
-
-
     @GetMapping("/")//代表根目录
     public String index(HttpServletRequest request){
         Cookie[] cookies=request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")){
-                String token=cookie.getValue();
-                User user=userMapper.findByToken(token);
-                if (user!=null){
-                    request.getSession().setAttribute("user",user);//request把user信息保存到session中
+        if(cookies!=null&&cookies.length>0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);//request把user信息保存到session中
+                    }
+                    break;
                 }
-                break;
             }
         }
         return "index";//此时会在摸板目录找
